489c72a1226944c8593489cb0eb81bc044668068
Jeniffer Bittencourt <lo.jbittencourt@gmail.com>
2023-08-21 15:36:58 -0300

    video 2.1

--- src/main/kotlin/br/com/alura/alugames/dados/Banco.kt
+++ src/main/kotlin/br/com/alura/alugames/dados/Banco.kt
@@ -1,17 +1,21 @@
 package br.com.alura.alugames.dados
 
-import br.com.alura.alugames.modelo.Jogo
-import java.sql.Connection
-import java.sql.DriverManager
-import java.sql.SQLException
+import javax.persistence.EntityManager
+import javax.persistence.EntityManagerFactory
+import javax.persistence.Persistence
 
 object Banco {
-    fun obterConexao(): Connection? {
-        return try {
-            DriverManager.getConnection("jdbc:mysql://localhost:3306/alugames", "root", "aLxXzDcgueZu6z")
-        } catch (e: SQLException) {
-            e.printStackTrace()
-            null
-        }
+//    fun obterConexao(): Connection? {
+//        return try {
+//            DriverManager.getConnection("jdbc:mysql://localhost:3306/alugames", "root", "aLxXzDcgueZu6z")
+//        } catch (e: SQLException) {
+//            e.printStackTrace()
+//            null
+//        }
+//    }
+
+    fun getEntityManager(): EntityManager {
+        val factory: EntityManagerFactory = Persistence.createEntityManagerFactory("alugames")
+        return factory.createEntityManager()
     }
 }
