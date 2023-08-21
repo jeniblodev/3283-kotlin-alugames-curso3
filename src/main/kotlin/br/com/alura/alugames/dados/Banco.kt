f43a4a1a9e3d650108be5c2e8689f30d5e57038c
Jeniffer Bittencourt <lo.jbittencourt@gmail.com>
2023-08-21 14:29:07 -0300

    video 1.4

--- src/main/kotlin/br/com/alura/alugames/dados/Banco.kt
+++ src/main/kotlin/br/com/alura/alugames/dados/Banco.kt
@@ -1,5 +1,6 @@
 package br.com.alura.alugames.dados
 
+import br.com.alura.alugames.modelo.Jogo
 import java.sql.Connection
 import java.sql.DriverManager
 import java.sql.SQLException
@@ -12,5 +13,33 @@ object Banco {
             e.printStackTrace()
             null
         }
+    }
+    fun getJogos(): List<Jogo> {
+        val listaJogos = mutableListOf<Jogo>()
+        val conexao = obterConexao()
+
+        if (conexao != null) {
+            try {
+                val statement = conexao.createStatement()
+                val resultado = statement.executeQuery("SELECT * FROM JOGOS")
+
+                while (resultado.next()) {
+                    val id = resultado.getInt("id")
+                    val titulo = resultado.getString("titulo")
+                    val capa = resultado.getString("capa")
+                    val descricao = resultado.getString("descricao")
+                    val preco = resultado.getDouble("preco")
+
+                    val jogo = Jogo(titulo,capa, preco, descricao, id)
+                    listaJogos.add(jogo)
+                }
+
+                statement.close()
+            } finally {
+                conexao.close()
+            }
+        }
+
+        return listaJogos
     }
 }
--- src/main/kotlin/br/com/alura/alugames/principal/testeBanco.kt
+++ src/main/kotlin/br/com/alura/alugames/principal/testeBanco.kt
@@ -1,8 +1,9 @@
 package br.com.alura.alugames.principal
 
 import br.com.alura.alugames.dados.Banco
+import br.com.alura.alugames.modelo.Jogo
 
 fun main() {
-    val conexao = Banco.obterConexao()
-    println(conexao)
+    val listaJogos: List<Jogo> = Banco.getJogos()
+    println(listaJogos)
 }
