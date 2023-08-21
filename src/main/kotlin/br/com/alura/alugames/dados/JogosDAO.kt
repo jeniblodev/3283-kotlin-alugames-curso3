5cbd1c3477fadeefa9a3b083eab17315db5039fe
Jeniffer Bittencourt <lo.jbittencourt@gmail.com>
2023-08-21 17:16:01 -0300

    video 2.4

--- src/main/kotlin/br/com/alura/alugames/dados/JogosDAO.kt
+++ src/main/kotlin/br/com/alura/alugames/dados/JogosDAO.kt
@@ -1,66 +1,21 @@
 package br.com.alura.alugames.dados
 
 import br.com.alura.alugames.modelo.Jogo
+import javax.persistence.EntityManager
 
-class JogosDAO {
+class JogosDAO(val manager: EntityManager) {
 
     fun getJogos(): List<Jogo> {
-        val manager = Banco.getEntityManager()
-        try {
-            val query = manager.createQuery("FROM JogoEntity", JogoEntity::class.java)
-            return query.resultList.map { entity -> Jogo(entity.titulo, entity.capa, entity.preco, entity.descricao, entity.id) }
-        } finally {
-            manager.close()
-        }
+        val query = manager.createQuery("FROM JogoEntity", JogoEntity::class.java)
+        return query.resultList.map { entity -> Jogo(entity.titulo, entity.capa, entity.preco, entity.descricao, entity.id) }
+    }
+
+    fun adicionarJogo(jogo: Jogo) {
+        val entity = JogoEntity(jogo.titulo, jogo.capa, jogo.preco, jogo.descricao)
+        manager.transaction.begin()
+        manager.persist(entity)
+        manager.transaction.commit()
     }
 
 
-//    fun getJogos(): List<Jogo> {
-//        val listaJogos = mutableListOf<Jogo>()
-//        val conexao = Banco.obterConexao()
-//
-//        if (conexao != null) {
-//            try {
-//                val statement = conexao.createStatement()
-//                val resultado = statement.executeQuery("SELECT * FROM JOGOS")
-//
-//                while (resultado.next()) {
-//                    val id = resultado.getInt("id")
-//                    val titulo = resultado.getString("titulo")
-//                    val capa = resultado.getString("capa")
-//                    val descricao = resultado.getString("descricao")
-//                    val preco = resultado.getDouble("preco")
-//
-//                    val jogo = Jogo(titulo,capa, preco, descricao, id)
-//                    listaJogos.add(jogo)
-//                }
-//
-//                statement.close()
-//            } finally {
-//                conexao.close()
-//            }
-//        }
-//
-//        return listaJogos
-//    }
-//    fun adicionarJogo(jogo: Jogo) {
-//        val conexao = Banco.obterConexao()
-//        val insert = "INSERT INTO JOGOS (TITULO, CAPA, PRECO, DESCRICAO) VALUES (?, ?, ?, ?)"
-//
-//        if (conexao != null) {
-//            try {
-//                val statement = conexao.prepareStatement(insert)
-//                statement.setString(1, jogo.titulo)
-//                statement.setString(2, jogo.capa)
-//                statement.setDouble(3, jogo.preco)
-//                statement.setString(4, jogo.descricao)
-//
-//                statement.executeUpdate()
-//                statement.close()
-//
-//            } finally {
-//                conexao.close()
-//            }
-//        }
-//    }
 }
--- src/main/kotlin/br/com/alura/alugames/principal/testeBanco.kt
+++ src/main/kotlin/br/com/alura/alugames/principal/testeBanco.kt
@@ -6,9 +6,14 @@ import br.com.alura.alugames.modelo.Jogo
 
 fun main() {
     val jogo = Jogo("The Last of Us Part I", "https://cdn.cloudflare.steamstatic.com/steam/apps/1888930/header.jpg?t=1686864554", 5.99,"Uma aventura pós-apocalíptica de sobrevivência em um mundo infestado por zumbis e facções em conflito.")
-    val jogoDAO = JogosDAO()
-//    jogoDAO.adicionarJogo(jogo)
+    val jogo2 = Jogo("Dandara", "https://cdn.cloudflare.steamstatic.com/steam/apps/612390/header.jpg?t=1674055293", 9.99,"Um jogo de plataforma e ação com elementos de metroidvania, onde você controla a heroína Dandara em sua luta para libertar um mundo repleto de opressão e tirania.")
+
+    val manager = Banco.getEntityManager()
+    val jogoDAO = JogosDAO(manager)
+    jogoDAO.adicionarJogo(jogo2)
 
     val listaJogos: List<Jogo> = jogoDAO.getJogos()
     println(listaJogos)
+
+    manager.close()
 }
