0f33ba920afd2ed4689a05d121306a7478e9feb3
Jeniffer Bittencourt <lo.jbittencourt@gmail.com>
2023-08-22 00:10:41 -0300

    Vamos praticar? - Faça como eu fiz

--- src/main/kotlin/br/com/alura/alugames/dados/GamersDAO.kt
+++ src/main/kotlin/br/com/alura/alugames/dados/GamersDAO.kt
@@ -3,17 +3,13 @@ package br.com.alura.alugames.dados
 import br.com.alura.alugames.modelo.Gamer
 import javax.persistence.EntityManager
 
-class GamersDAO(private val manager: EntityManager) {
+class GamersDAO(manager: EntityManager): DAO<Gamer, GamerEntity>(manager, GamerEntity::class.java) {
 
-    fun getGamers(): List<Gamer> {
-        val query = manager.createQuery("FROM GamerEntity", GamerEntity::class.java)
-        return query.resultList.map { entity -> Gamer(entity.nome, entity.email, entity.dataNascimento, entity.usuario, entity.id) }
+    override fun toEntity(objeto: Gamer): GamerEntity {
+        return GamerEntity(objeto.id, objeto.nome, objeto.email, objeto.dataNascimento, objeto.usuario)
     }
 
-    fun adicionarGamer(gamer: Gamer) {
-        val entity = GamerEntity(gamer.id, gamer.nome, gamer.email, gamer.dataNascimento, gamer.usuario)
-        manager.transaction.begin()
-        manager.persist(entity)
-        manager.transaction.commit()
+    override fun toModel(entity: GamerEntity): Gamer {
+        return Gamer(entity.nome, entity.email, entity.dataNascimento, entity.usuario, entity.id)
     }
 }
