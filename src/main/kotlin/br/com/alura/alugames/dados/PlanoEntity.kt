37288de8cdb3493286c5ff2e77d77632993f5f67
Jeniffer Bittencourt <lo.jbittencourt@gmail.com>
2023-08-22 00:29:28 -0300

    video 4.1

--- src/main/kotlin/br/com/alura/alugames/dados/PlanoEntity.kt
+++ src/main/kotlin/br/com/alura/alugames/dados/PlanoEntity.kt
@@ -1,9 +1,26 @@
 package br.com.alura.alugames.dados
 
+import javax.persistence.DiscriminatorColumn
+import javax.persistence.DiscriminatorType
+import javax.persistence.DiscriminatorValue
+import javax.persistence.Entity
+import javax.persistence.Inheritance
+import javax.persistence.InheritanceType
+import javax.persistence.Table
+
+
+@Entity
+@Table(name = "planos")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "TipoPlano", discriminatorType = DiscriminatorType.STRING)
 sealed class PlanoEntity(val tipo: String)
 
+@Entity
+@DiscriminatorValue("Avulso")
 class PlanoAvulsoEntity(tipo: String): PlanoEntity(tipo)
 
+@Entity
+@DiscriminatorValue("Assinatura")
 class PlanoAssinaturaEntity(
     tipo: String,
     val mensalidade: Double,
